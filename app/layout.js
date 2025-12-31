7d2285b452b666f33e705ffd82b579e9a24b1d4c
pebe123 <61807246+pebe123@users.noreply.github.com>
2025-12-31 21:39:35 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
-      <head>
-        <script src="https://cdn.tailwindcss.com"></script>
-      </head>
-      <body style={{ margin: 0, padding: 0 }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
